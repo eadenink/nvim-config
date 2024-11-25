bfc1599f647352d08d5f54d492c5123cbe3bdfa4
Alexander Orlov <eadenink@gmail.com>
2024-11-25 14:08:05 +0400

    feat: Update colorscheme

--- lua/eadenink/init.lua
+++ lua/eadenink/init.lua
@@ -1,1 +1,2 @@
 require("eadenink.remap")
+require("eadenink.colorscheme")
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,8 +3,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end,
+    opts = {},
   },
 }

bf34b7f347b2c7d013633aff4a5576a0a360629d
Alexander Orlov <eadenink@gmail.com>
2024-11-29 20:54:23 +0400

    feat: Update colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,8 +1,16 @@
-return {
-  {
-    "folke/tokyonight.nvim",
+local function colorscheme(name, opts)
+  return {
+    name,
     lazy = false,
     priority = 1000,
-    opts = {},
-  },
+    opts = opts or {},
+  }
+end
+
+return {
+  colorscheme("folke/tokyonight.nvim"),
+  colorscheme("rebelot/kanagawa.nvim"),
+  colorscheme("rose-pine/neovim"),
+  colorscheme("catppuccin/nvim"),
+  colorscheme("EdenEast/nightfox.nvim"),
 }

23e3e226ef09a50f34b0ab22e6524030f13e3660
Alexander Orlov <eadenink@gmail.com>
2024-12-03 01:29:27 +0400

    feat: Remove unused colorschemas

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -9,8 +9,5 @@ end
 
 return {
   colorscheme("folke/tokyonight.nvim"),
-  colorscheme("rebelot/kanagawa.nvim"),
-  colorscheme("rose-pine/neovim"),
-  colorscheme("catppuccin/nvim"),
   colorscheme("EdenEast/nightfox.nvim"),
 }

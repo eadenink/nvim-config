df5d4bc9964ee8c3b07cdbaeb00cafb013e39aed
Alexander Orlov <eadenink@gmail.com>
2025-01-06 06:39:53 +0400

    feat: Extend telescope plugin with ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,28 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>pgf', builtin.git_files, { desc = 'Find files within git' })
-    vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Search for the string within working dir' })
-    vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = 'Search for the word under the cursor within working dir' })
-  end
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files' })
+		vim.keymap.set('n', '<leader>pgf', builtin.git_files, { desc = 'Find files within git' })
+		vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Search for the string within working dir' })
+		vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = 'Search for the word under the cursor within working dir' })
+		end
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+						}
+					}
+				}
+			})
+			require("telescope").load_extension("ui-select")
+		end
+	},
 }

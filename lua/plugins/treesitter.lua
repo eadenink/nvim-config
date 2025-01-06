2d2e3db30a84462c78424b105a5697489cea9820
Alexander Orlov <eadenink@gmail.com>
2025-01-06 08:02:06 +0400

    feat: Use treesitter autoinstall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,52 +1,18 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        highlight = {
-          enable = true,
-        },
-	indent = {
-	  enable = true,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+				auto_install = true,
+			})
+		end,
 	},
-        auto_install = true,
-        ensure_installed = {
-          "c",
-          "csv",
-          "bash",
-          "dockerfile",
-          "gitignore",
-          "go",
-          "gomod",
-          "gosum",
-          "graphql",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "lua",
-          "luadoc",
-          "luap",
-          "markdown",
-          "markdown_inline",
-          "nginx",
-          "prisma",
-          "proto",
-          "python",
-          "regex",
-          "rust",
-          "sql",
-          "toml",
-          "tsx",
-          "typescript",
-          "xml",
-          "yaml",
-          "zig",
-        },
-      }
-    end,
-  },
 }
-

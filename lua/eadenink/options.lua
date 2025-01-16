c6a175588de4007bf49ed533e6125de3f589335f
Alexander Orlov <eadenink@gmail.com>
2025-01-16 15:56:27 +0400

    feat: Update options

--- lua/eadenink/options.lua
+++ lua/eadenink/options.lua
@@ -1,24 +1,33 @@
 local opt = vim.opt
 
+-- Makes cursor permanently bold
 opt.guicursor = ""
 
+-- Lines numbering
 opt.nu = true
 opt.relativenumber = true
 
+-- Indentation
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
+opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
 
+-- Undo
 opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
+-- Search
 opt.hlsearch = false
 opt.incsearch = true
+opt.ingorecase = true
+opt.smartcase = true
 
+-- Better colors and cursor line highlighting
 opt.termguicolors = true
 opt.cursorline = true
 
